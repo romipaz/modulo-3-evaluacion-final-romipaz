e5fd9700b6fb31bc9936d999129ebd5c6e9b7785
rpaz-code <eccoromi@gmail.com>
2020-04-25 22:37:58 +0200

    add docs

---
+++ docs/precache-manifest.6394839d9d59374c1eda067798444d20.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f96c782ebf0d6831036c5b047529188",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bb056bc146ba33bf50ca",
+    "url": "/static/css/main.65ce7bb8.chunk.css"
+  },
+  {
+    "revision": "6057549439c621cce577",
+    "url": "/static/js/2.aedec679.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.aedec679.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bb056bc146ba33bf50ca",
+    "url": "/static/js/main.c0ae9d61.chunk.js"
+  },
+  {
+    "revision": "46a82b834a2f347623fc",
+    "url": "/static/js/runtime-main.8a01c2dc.js"
+  },
+  {
+    "revision": "bbd33f557e371e22f67c60681a07eebb",
+    "url": "/static/media/faces.bbd33f55.gif"
+  },
+  {
+    "revision": "75509f2ec9725b3cef07eefe651a4a8f",
+    "url": "/static/media/rmlogo.75509f2e.png"
+  },
+  {
+    "revision": "e5d47e7f787cc2f99dd2094df732003f",
+    "url": "/static/media/rmwallpaper2.e5d47e7f.png"
+  }
+]);
